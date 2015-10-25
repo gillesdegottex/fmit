6eac2d1e19c587b2f6aa5f62096d6f22d8f1fae5
Gilles Degottex <gilles.degottex@gmail.com>
2015-10-25 13:48:35 +0000

    cleaning

--- distrib/package_appveyor_Win64bit.ps1
+++ distrib/package_appveyor_Win64bit.ps1
@@ -40,7 +40,7 @@ cd ..
 
 #"C:/Program Files (x86)/Inno Setup 5/ISCC"
 
-& "c:\Program Files (x86)\Inno Setup 5\ISCC.exe" /o. /dMyAppVersion=$VERSION c:\projects\fmit\distrib\FMIT_MSVC2012_OpenGL_Win64bit.iss
+& "c:\Program Files (x86)\Inno Setup 5\ISCC.exe" /o. /dMyAppVersion=$VERSION c:\projects\fmit\distrib\FMIT_MSVC2012_Win64bit.iss
 
 # Get out of distrib
 cd ..
