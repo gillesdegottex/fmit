54409b0d628af44bfacbad159753993ddbf11c43
Gilles Degottex <gilles.degottex@gmail.com>
2016-07-10 12:42:43 +0100

    add Depends

--- distrib/package_deb.sh
+++ distrib/package_deb.sh
@@ -2,31 +2,45 @@ BINFILE=$1
 
 VERSION=`git describe --tags --always |cut -c2-`-1github1
 ARCH=amd64
+DISTRIB=`lsb_release -d |sed 's/Description:\s//g' |sed 's/\s/_/g'`
 
-PKGNAME=fmit-$VERSION
+echo "Packaging FMIT "$VERSION
+echo "Distribution: "$DISTRIB
+echo "Package name "$PKGNAME
+BINARCH=`objdump -p $BINFILE |grep 'file format' |sed 's/^.*file format\s//g'`
+if [ "${BINARCH/elf64}" = "${BINARCH}" ] ; then
+    ARCH=i386
+else
+    ARCH=amd64
+fi
+echo "Architecture: "$ARCH
 
-echo "Packaging FMIT "$VERSION" in "$PKGNAME
-echo "List dependencies:"
-objdump -p $BINFILE |grep 'file format'
-# TODO
-# DEPS=`objdump -p $BINFILE |grep NEEDED |awk '{ print $2 }'`
-# for dep in $DEPS; do
-#     deplist=`dpkg -S $dep |sed 's/:.*$//g'`
-#     depdpkg=`echo "$deplist" |uniq`
-#     depcurver=`dpkg -s $depdpkg |grep 'Version' |awk '{ print $2 }' |sed 's/:.*$//g' |sed 's/-.*$//g' |sed 's/+.*$//g'`
-#     echo "Dependency "$dep":"$depdpkg":"$depcurver
-# done
-
-# Start to build the files tree
+PKGNAME=fmit-$VERSION-$DISTRIB
 
 rm -fr package_deb_$PKGNAME
 mkdir -p package_deb_$PKGNAME
+
+echo "List dependencies:"
+
+DEPS=`objdump -p $BINFILE |grep NEEDED |awk '{ print $2 }'`
+for dep in $DEPS; do
+    deplist=`dpkg -S $dep |sed 's/:.*$//g'`
+    depdpkg=`echo "$deplist" |sort |uniq`
+    depcurver=`dpkg -s $depdpkg |grep 'Version' |awk '{ print $2 }' |sed 's/:.*$//g' |sed 's/-.*$//g' |sed 's/+.*$//g'`
+    echo "Dependency "$dep"    in package:"$depdpkg"    version:"$depcurver
+    echo $depdpkg >> package_deb_$PKGNAME/Depends
+done
+DEPENDS=`cat package_deb_$PKGNAME/Depends |sort |uniq`
+DEPENDS=`echo $DEPENDS |sed 's/ /,\ /g'`
+
+# Start to build the files tree
 
 # Copy files
 mkdir -p package_deb_$PKGNAME/$PKGNAME/DEBIAN
 cp package_deb.control package_deb_$PKGNAME/$PKGNAME/DEBIAN/control
 sed -i "s/^Version:.*$/Version: $VERSION/g" package_deb_$PKGNAME/$PKGNAME/DEBIAN/control
 sed -i "s/^Architecture:.*$/Architecture: $ARCH/g" package_deb_$PKGNAME/$PKGNAME/DEBIAN/control
+sed -i "s/^Depends:.*$/Depends: $DEPENDS/g" package_deb_$PKGNAME/$PKGNAME/DEBIAN/control
 
 # The binary
 mkdir -p package_deb_$PKGNAME/$PKGNAME/usr/bin
@@ -59,3 +73,5 @@ mkdir -p package_deb_$PKGNAME/$PKGNAME/usr/share/fmit/scales
 cp ../scales/* package_deb_$PKGNAME/$PKGNAME/usr/share/fmit/scales/
 
 dpkg-deb --build package_deb_$PKGNAME/$PKGNAME
+
+ls package_deb_$PKGNAME/$PKGNAME.deb
