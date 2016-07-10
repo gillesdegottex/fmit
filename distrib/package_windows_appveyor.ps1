9263ee056d87a8a0fd523d4018f47211a14f0f48
Gilles Degottex <gilles.degottex@gmail.com>
2016-07-10 22:52:47 +0100

    cleaning

--- distrib/package_windows_appveyor.ps1
+++ distrib/package_windows_appveyor.ps1
@@ -2,36 +2,36 @@ $arch=$args[0]
 
 #http://doc.qt.io/qt-5/windows-deployment.html
 $VERSION = ((git describe --tags --always) | Out-String).Substring(1)
-$VERSION = $VERSION -replace "`n|`r"
-echo "Version: $VERSION"
+$VERSION = ${VERSION} -replace "`n|`r"
+echo "Version: ${VERSION}"
 
 If ($arch -eq 'x64') {
     echo "Packaging for 64bits Windows"
-    $PACKAGENAME = "fmit_$VERSION`_win64bit"
+    $PACKAGENAME = "fmit_${VERSION}_win64bit"
     $QTPATH = "\Qt\5.4\msvc2013_64_opengl"
 }
 Else {
     echo "Packaging for 32bits Windows"
-    $PACKAGENAME = "fmit_$VERSION`_win32bit"
+    $PACKAGENAME = "fmit_${VERSION}_win32bit"
     $QTPATH = "\Qt\5.4\msvc2013_opengl"
 }
 
-echo "Packaging $PACKAGENAME"
+echo "Packaging ${PACKAGENAME}"
 echo " "
 
 cd distrib
 
 # Create a directory for the files to package
-New-Item -ItemType directory -Name $PACKAGENAME | Out-Null
+New-Item -ItemType directory -Name ${PACKAGENAME} | Out-Null
 
 # Add the executable
-Copy-Item c:\projects\fmit\release\fmit.exe $PACKAGENAME
+Copy-Item c:\projects\fmit\release\fmit.exe ${PACKAGENAME}
 
 # Add external libraries
-Copy-Item c:\projects\fmit\lib\libfft\libfftw3-3.dll $PACKAGENAME
+Copy-Item c:\projects\fmit\lib\libfft\libfftw3-3.dll ${PACKAGENAME}
 
 # Add the Qt related libs, qt translations and installer of MSVC redist.
-cd $PACKAGENAME
+cd ${PACKAGENAME}
 #C:/Qt/5.4/msvc2013_opengl/bin/qtenv2.bat
 #export PATH=\C$QTPATH\bin:$PATH
 #echo $PATH
