d205df6fc840d23b874e41ae8efc856d5b9da4a6
Davi da Silva Böger <dsboger@gmail.com>
2018-05-02 14:09:59 -0300

    Fix note names selection

--- src/CustomInstrumentTunerForm.cpp
+++ src/CustomInstrumentTunerForm.cpp
@@ -818,9 +818,9 @@ void CustomInstrumentTunerForm::configure_ok()
     else if(m_config_form.ui_cbTonality->currentIndex()==2)	SetTonality(-3);
     else if(m_config_form.ui_cbTonality->currentIndex()==3)	SetTonality(-5);
 
-    if(m_config_form.ui_cbNotesName->currentIndex()==0)		SetNotesName(LOCAL_ANGLO);
+	if(m_config_form.ui_cbNotesName->currentIndex()==0)		SetNotesName(LOCAL_ANGLO);
 	if(m_config_form.ui_cbNotesName->currentIndex()==1)		SetNotesName(LOCAL_LATIN);
-	else													SetNotesName(LOCAL_GERMAN);
+	if(m_config_form.ui_cbNotesName->currentIndex()==2)		SetNotesName(LOCAL_GERMAN);
 	m_microtonalView->notesNameChanged();
 	m_microtonalView->setAFreq(Music::GetAFreq());
 
