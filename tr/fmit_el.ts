4b8492f1f1af49dcdf042a4f21267b671c96cfa9
Michalis <michalisntovas@yahoo.gr>
2021-02-07 09:34:12 +0000

    Translated using Weblate (Greek)
    Currently translated at 100.0% (201 of 201 strings)
    
    Translation: FMIT/Translations
    Translate-URL: https://hosted.weblate.org/projects/fmit/translations/el/

--- tr/fmit_el.ts
+++ tr/fmit_el.ts
@@ -33,7 +33,7 @@ p, li { white-space: pre-wrap; }
     </message>
     <message>
         <source>Credits</source>
-        <translation>Έπαινοι</translation>
+        <translation>Μνείες</translation>
     </message>
     <message>
         <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
